ce7b304a48f8bd8dae275fa006deea14fa98fc24
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-03 18:36:42 -0600

    Bring in the Zoho SDK.

---
+++ Zoho/src/main/java/co/casterlabs/zohoapijava/ThreadHelper.java
@@ -0,0 +1,31 @@
+package co.casterlabs.zohoapijava;
+
+public class ThreadHelper {
+    private static int threadCount = 0;
+
+    public static void executeAsync(String name, Runnable run) {
+        Thread t = new Thread(run);
+
+        t.setName(name + " - ZohoApi Async Thread #" + threadCount++);
+        t.start();
+    }
+
+    public static Thread executeAsyncLater(String name, Runnable run, long millis) {
+        Thread t = (new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(millis);
+                    run.run();
+                } catch (InterruptedException ignored) {} // We ignore this so we can cancel waits.
+            }
+        });
+
+        t.setName(name + " - ZohoApi Waiting Thread #" + threadCount++);
+        t.setDaemon(true);
+        t.start();
+
+        return t;
+    }
+
+}
---
+++ Zoho/src/main/java/co/casterlabs/zohoapijava/types/ZohoEmailAddress.java
@@ -0,0 +1,16 @@
+package co.casterlabs.zohoapijava.types;
+
+import co.casterlabs.rakurai.json.annotating.JsonClass;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@JsonClass(exposeAll = true)
+public class ZohoEmailAddress {
+    private boolean isAlias;
+    private boolean isPrimary;
+    private String mailId;
+    private boolean isConfirmed;
+
+}
