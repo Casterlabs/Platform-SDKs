3ea8ae1ac6cb4d05b74a0e29e19ba34ca3a570e0
Ender <33337309+e3ndr@users.noreply.github.com>
2023-04-25 18:21:18 -0500

    Kick: Get badges working again.

--- Kick/src/main/java/co/casterlabs/sdk/kick/realtime/types/KickChatSender.java
+++ Kick/src/main/java/co/casterlabs/sdk/kick/realtime/types/KickChatSender.java
@@ -1,8 +1,14 @@
 package co.casterlabs.sdk.kick.realtime.types;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import co.casterlabs.rakurai.json.annotating.JsonClass;
+import co.casterlabs.sdk.kick.realtime.types.KickChatSender.Identity.Badge;
 import lombok.Getter;
 import lombok.ToString;
+import xyz.e3ndr.fastloggingframework.logging.FastLogger;
+import xyz.e3ndr.fastloggingframework.logging.LogLevel;
 
 @Getter
 @ToString
@@ -21,69 +27,68 @@ public class KickChatSender {
     @JsonClass(exposeAll = true)
     public static class Identity {
         private String color;
-//        private List<Badge> badges;
-//
-//        @Getter
-//        @ToString
-//        @JsonClass(exposeAll = true)
-//        public static class Badge {
-//            private String type;
-//            private String text;
-//        }
+        private List<Badge> badges;
+
+        @Getter
+        @ToString
+        @JsonClass(exposeAll = true)
+        public static class Badge {
+            private String type;
+            private String text;
+        }
     }
 
     /**
      * @return a list of URLs with SVG images.
      */
-//    public List<String> getBadgeUrls() {
-//        List<String> badges = new LinkedList<>();
-//
-//        // TODO Keep this in-sync with the badges in the GitHub repo.
-//
-//        // Role
-//        if (this.role != null) {
-//            switch (this.role) {
-//                case "Channel Host":
-//                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Roles/Channel%20Host.svg");
-//                    break;
-//                case "Moderator":
-//                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Roles/Moderator.svg");
-//                    break;
-//            }
-//        }
-//        if (this.followerBadges.contains("OG")) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Follower%20Badges/OG.svg");
-//        }
-//        if (this.followerBadges.contains("VIP")) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Follower%20Badges/VIP.svg");
-//        }
-//
-//        // Follower Badges
-//        if (this.followerBadges.contains("OG")) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Follower%20Badges/OG.svg");
-//        }
-//        if (this.followerBadges.contains("VIP")) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Follower%20Badges/VIP.svg");
-//        }
-//
-//        // Misc.
-//        if (this.isSubscribed) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Other/is_subscribed.svg");
-//        }
-//        if (this.isVerified) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Other/verified.svg");
-//        }
-//        if (this.isSuperAdmin) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Other/isSuperAdmin.svg");
-//        }
-//        if (this.isFounder) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Other/is_founder.svg");
-//        }
-//        if (this.username.equalsIgnoreCase("TrainwrecksTV")) {
-//            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Other/TrainwrecksTV.svg");
-//        }
-//
-//        return badges;
-//    }
+    public List<String> getBadgeUrls() {
+        List<String> badges = new LinkedList<>();
+
+        for (Badge badge : this.identity.badges) {
+            switch (badge.type) {
+                case "broadcaster":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/broadcaster.svg");
+                    break;
+
+                case "moderator":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/moderator.svg");
+                    break;
+
+                case "vip":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/vip.svg");
+                    break;
+
+                case "og":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/og.svg");
+                    break;
+
+                case "verified":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/verified.svg");
+                    break;
+
+                case "founder":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/founder.svg");
+                    break;
+
+                case "staff":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/staff.svg");
+                    break;
+
+                case "subscriber":
+                    badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/subscriber.svg");
+                    break;
+
+                default:
+                    FastLogger.logStatic(LogLevel.WARNING, "Unknown badge type: %s ('%s')", badge.type, badge.text);
+                    break;
+            }
+        }
+
+        if (this.username.equalsIgnoreCase("TrainwrecksTV")) {
+            badges.add("https://raw.githubusercontent.com/Casterlabs/Platform-SDKs/main/Kick/Badges/Other/TrainwrecksTV.svg");
+        }
+
+        return badges;
+    }
 
 }
