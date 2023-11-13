976c028cfaec1c4aebfe8ffb9f6a73c5e503aee6
Ender <33337309+e3ndr@users.noreply.github.com>
2023-11-13 03:25:44 -0600

    TikTok: Looks like we gotta use us.

--- TikTok/src/main/java/co/casterlabs/sdk/tiktok/unsupported/webcast/WebcastConstants.java
+++ TikTok/src/main/java/co/casterlabs/sdk/tiktok/unsupported/webcast/WebcastConstants.java
@@ -2,6 +2,6 @@ package co.casterlabs.sdk.tiktok.unsupported.webcast;
 
 public class WebcastConstants {
     public static String TIKTOK_SITE_URL = "https://tiktok.com";
-    public static String TIKTOK_WEBCAST_URL = "https://webcast.tiktok.com";
+    public static String TIKTOK_WEBCAST_URL = "https://webcast.us.tiktok.com";
 
 }
