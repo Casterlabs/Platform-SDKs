357314032947d0e0e3ed36134d9e3dba2df41f42
Ender <33337309+e3ndr@users.noreply.github.com>
2023-11-28 04:42:34 -0600

    Base: Update how the rate limiters work.

--- _Base/src/main/java/co/casterlabs/apiutil/ratelimit/BucketRateLimiter.java
+++ _Base/src/main/java/co/casterlabs/apiutil/ratelimit/BucketRateLimiter.java
@@ -4,11 +4,41 @@ import java.lang.ref.WeakReference;
 import java.util.concurrent.TimeUnit;
 
 import co.casterlabs.commons.async.AsyncTask;
+import lombok.SneakyThrows;
 
-public class BucketRateLimiter extends RateLimiter {
+public class BucketRateLimiter {
+    private int available = 0;
+    private int waiting = 0;
 
     public BucketRateLimiter(int bucketSize, int refillRate, TimeUnit unit) {
         doRefill(new WeakReference<>(this), bucketSize, refillRate, unit);
+    }
+
+    @SneakyThrows
+    public synchronized void block() {
+        if (this.available == 0) {
+            this.waiting++;
+            this.wait();
+            this.waiting--;
+        }
+
+        this.available--;
+
+        if (this.available > 0) {
+            this.notify();
+        }
+    }
+
+    public synchronized void setAvailable(int value) {
+        this.available = value;
+
+        this.notify();
+    }
+
+    public synchronized void addAvailable(int value) {
+        this.available += value;
+
+        this.notify();
     }
 
     private static void doRefill(WeakReference<BucketRateLimiter> $ref, int bucketSize, int refillRate, TimeUnit unit) {
