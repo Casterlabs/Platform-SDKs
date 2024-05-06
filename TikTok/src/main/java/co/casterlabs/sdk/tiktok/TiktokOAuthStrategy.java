0969fdcbaa56f379fd2f5cee07cd6cdd13a9e1d4
Ender <33337309+e3ndr@users.noreply.github.com>
2024-05-06 03:04:31 -0500

    TikTok: I feel like an idiot.
    RTFM.

--- TikTok/src/main/java/co/casterlabs/sdk/tiktok/TiktokOAuthStrategy.java
+++ TikTok/src/main/java/co/casterlabs/sdk/tiktok/TiktokOAuthStrategy.java
@@ -1,22 +1,14 @@
 package co.casterlabs.sdk.tiktok;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.NoSuchElementException;
-import java.util.UUID;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import org.unbescape.uri.UriEscape;
 
 import co.casterlabs.apiutil.auth.ApiAuthException;
 import co.casterlabs.apiutil.auth.OAuthProvider.OAuthResponse;
@@ -78,16 +70,18 @@ public class TiktokOAuthStrategy implements OAuthStrategy {
     }
 
     protected static JsonObject sendAuthHttp(Map<String, String> body, String url) throws IOException {
-        MultiPartBodyPublisher form = new MultiPartBodyPublisher();
-        for (Entry<String, String> entry : body.entrySet()) {
-            form.addPart(entry.getKey(), entry.getValue());
-        }
-
         return WebRequest.sendHttpRequest(
             HttpRequest.newBuilder()
                 .uri(URI.create(url))
-                .POST(form.build())
-                .header("Content-Type", form.getBoundary()),
+                .POST(
+                    BodyPublishers.ofString(
+                        body.entrySet()
+                            .stream()
+                            .map((e) -> UriEscape.escapeUriQueryParam(e.getKey()) + "=" + UriEscape.escapeUriQueryParam(e.getValue()))
+                            .collect(Collectors.joining("&"))
+                    )
+                )
+                .header("Content-Type", "application/x-www-form-urlencoded"),
             RsonBodyHandler.of(JsonObject.class),
             null
         ).body();
@@ -103,160 +97,4 @@ public class TiktokOAuthStrategy implements OAuthStrategy {
         }
     }
 
-}
-
-class MultiPartBodyPublisher {
-    private List<PartsSpecification> partsSpecificationList = new ArrayList<>();
-    private String boundary = UUID.randomUUID().toString();
-
-    public HttpRequest.BodyPublisher build() {
-        if (partsSpecificationList.size() == 0) {
-            throw new IllegalStateException("Must have at least one part to build multipart message.");
-        }
-        addFinalBoundaryPart();
-        return HttpRequest.BodyPublishers.ofByteArrays(PartsIterator::new);
-    }
-
-    public String getBoundary() {
-        return boundary;
-    }
-
-    public MultiPartBodyPublisher addPart(String name, String value) {
-        PartsSpecification newPart = new PartsSpecification();
-        newPart.type = PartsSpecification.TYPE.STRING;
-        newPart.name = name;
-        newPart.value = value;
-        partsSpecificationList.add(newPart);
-        return this;
-    }
-
-    public MultiPartBodyPublisher addPart(String name, Path value) {
-        PartsSpecification newPart = new PartsSpecification();
-        newPart.type = PartsSpecification.TYPE.FILE;
-        newPart.name = name;
-        newPart.path = value;
-        partsSpecificationList.add(newPart);
-        return this;
-    }
-
-    public MultiPartBodyPublisher addPart(String name, Supplier<InputStream> value, String filename, String contentType) {
-        PartsSpecification newPart = new PartsSpecification();
-        newPart.type = PartsSpecification.TYPE.STREAM;
-        newPart.name = name;
-        newPart.stream = value;
-        newPart.filename = filename;
-        newPart.contentType = contentType;
-        partsSpecificationList.add(newPart);
-        return this;
-    }
-
-    private void addFinalBoundaryPart() {
-        PartsSpecification newPart = new PartsSpecification();
-        newPart.type = PartsSpecification.TYPE.FINAL_BOUNDARY;
-        newPart.value = "--" + boundary + "--";
-        partsSpecificationList.add(newPart);
-    }
-
-    static class PartsSpecification {
-
-        public enum TYPE {
-            STRING,
-            FILE,
-            STREAM,
-            FINAL_BOUNDARY
-        }
-
-        PartsSpecification.TYPE type;
-        String name;
-        String value;
-        Path path;
-        Supplier<InputStream> stream;
-        String filename;
-        String contentType;
-
-    }
-
-    class PartsIterator implements Iterator<byte[]> {
-
-        private Iterator<PartsSpecification> iter;
-        private InputStream currentFileInput;
-
-        private boolean done;
-        private byte[] next;
-
-        PartsIterator() {
-            iter = partsSpecificationList.iterator();
-        }
-
-        @Override
-        public boolean hasNext() {
-            if (done) return false;
-            if (next != null) return true;
-            try {
-                next = computeNext();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-            if (next == null) {
-                done = true;
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        public byte[] next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            byte[] res = next;
-            next = null;
-            return res;
-        }
-
-        private byte[] computeNext() throws IOException {
-            if (currentFileInput == null) {
-                if (!iter.hasNext()) return null;
-                PartsSpecification nextPart = iter.next();
-                if (PartsSpecification.TYPE.STRING.equals(nextPart.type)) {
-                    String part = "--" + boundary + "\r\n" +
-                        "Content-Disposition: form-data; name=" + nextPart.name + "\r\n" +
-                        "Content-Type: text/plain; charset=UTF-8\r\n\r\n" +
-                        nextPart.value + "\r\n";
-                    return part.getBytes(StandardCharsets.UTF_8);
-                }
-                if (PartsSpecification.TYPE.FINAL_BOUNDARY.equals(nextPart.type)) {
-                    return nextPart.value.getBytes(StandardCharsets.UTF_8);
-                }
-                String filename;
-                String contentType;
-                if (PartsSpecification.TYPE.FILE.equals(nextPart.type)) {
-                    Path path = nextPart.path;
-                    filename = path.getFileName().toString();
-                    contentType = Files.probeContentType(path);
-                    if (contentType == null) contentType = "application/octet-stream";
-                    currentFileInput = Files.newInputStream(path);
-                } else {
-                    filename = nextPart.filename;
-                    contentType = nextPart.contentType;
-                    if (contentType == null) contentType = "application/octet-stream";
-                    currentFileInput = nextPart.stream.get();
-                }
-                String partHeader = "--" + boundary + "\r\n" +
-                    "Content-Disposition: form-data; name=" + nextPart.name + "; filename=" + filename + "\r\n" +
-                    "Content-Type: " + contentType + "\r\n\r\n";
-                return partHeader.getBytes(StandardCharsets.UTF_8);
-            } else {
-                byte[] buf = new byte[8192];
-                int r = currentFileInput.read(buf);
-                if (r > 0) {
-                    byte[] actualBytes = new byte[r];
-                    System.arraycopy(buf, 0, actualBytes, 0, r);
-                    return actualBytes;
-                } else {
-                    currentFileInput.close();
-                    currentFileInput = null;
-                    return "\r\n".getBytes(StandardCharsets.UTF_8);
-                }
-            }
-        }
-    }
 }
