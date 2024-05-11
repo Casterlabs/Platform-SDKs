1791ba9f18830c37612134f1372fc57d23c106cc
Ender <33337309+e3ndr@users.noreply.github.com>
2024-05-11 00:52:44 -0500

    YouTube: Implement the new auth system.

--- YouTube/src/main/java/co/casterlabs/sdk/youtube/YoutubeAuth.java
+++ YouTube/src/main/java/co/casterlabs/sdk/youtube/YoutubeAuth.java
@@ -3,162 +3,173 @@ package co.casterlabs.sdk.youtube;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.jetbrains.annotations.Nullable;
 import org.unbescape.uri.UriEscape;
 
 import co.casterlabs.apiutil.auth.ApiAuthException;
+import co.casterlabs.apiutil.auth.AuthDataProvider;
+import co.casterlabs.apiutil.auth.AuthDataProvider.InMemoryAuthDataProvider;
 import co.casterlabs.apiutil.auth.AuthProvider;
 import co.casterlabs.apiutil.web.ApiException;
 import co.casterlabs.rakurai.json.Rson;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import co.casterlabs.rakurai.json.annotating.JsonField;
 import co.casterlabs.rakurai.json.element.JsonObject;
+import co.casterlabs.sdk.youtube.YoutubeAuth.YoutubeAuthData;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.ToString;
 
-public class YoutubeAuth extends AuthProvider {
+public class YoutubeAuth extends AuthProvider<YoutubeAuthData> {
+    private final Object lock = new Object();
+
     private @Getter String apiKey;
+    private @Getter @Nullable String clientId; // Null when application auth.
+    private @Nullable String clientSecret; // Null when application auth.
+    private boolean isApplicationAuth = false;
 
-    private @Getter String clientId;
+    /* ---------------- */
+    /* Construction     */
+    /* ---------------- */
 
-    private String clientSecret;
-
-    private long lastRefresh = 0;
-    private long expiresIn = 0;
-
-    private String refreshToken;
-    private String accessToken;
-
-    public YoutubeAuth(@NonNull String apiKey) {
-        this.apiKey = apiKey;
+    private YoutubeAuth(AuthDataProvider<YoutubeAuthData> dataProvider) {
+        super(dataProvider);
     }
 
-    public YoutubeAuth(@NonNull String apiKey, @NonNull String clientId, @NonNull String clientSecret, @NonNull String refreshToken) throws ApiAuthException {
-        this.apiKey = apiKey;
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        this.refreshToken = refreshToken;
-        this.refresh();
+    public static YoutubeAuth ofUser(AuthDataProvider<YoutubeAuthData> dataProvider, String apiKey, String clientId, String clientSecret) {
+        YoutubeAuth auth = new YoutubeAuth(dataProvider);
+        auth.apiKey = apiKey;
+        auth.clientId = clientId;
+        auth.clientSecret = clientSecret;
+        return auth;
     }
+
+    public static YoutubeAuth ofApplication(String apiKey) {
+        YoutubeAuth auth = new YoutubeAuth(new InMemoryAuthDataProvider<>(YoutubeAuthData.of(null)));
+        auth.apiKey = apiKey;
+        auth.isApplicationAuth = true;
+        return auth;
+    }
+
+    /* ---------------- */
+    /* Impl.            */
+    /* ---------------- */
 
     @Override
     protected void authenticateRequest0(@NonNull HttpRequest.Builder request) {
         if (!this.isApplicationAuth()) {
-            request.header("Authorization", "Bearer " + this.accessToken);
+            request.header("Authorization", String.join(" ", this.data().tokenType, this.data().accessToken));
         }
 
         // Inject the client key.
-        String url = request.build().uri().toString();
-        request.uri(URI.create(url + "&key=" + this.apiKey));
+        String uri = request.build().uri().toString();
+        if (uri.indexOf('?') == -1) {
+            request.uri(URI.create(uri + "?key=" + this.apiKey));
+        } else {
+            request.uri(URI.create(uri + "&key=" + this.apiKey));
+        }
     }
 
     @Override
     public void refresh() throws ApiAuthException {
-        if (!this.isApplicationAuth()) {
-            String form = String.format(
-                "grant_type=refresh_token"
-                    + "&refresh_token=%s"
-                    + "&client_id=%s"
-                    + "&client_secret=%s",
-                UriEscape.escapeUriQueryParam(this.refreshToken),
-                UriEscape.escapeUriQueryParam(this.clientId),
-                UriEscape.escapeUriQueryParam(this.clientSecret)
-            );
+        if (this.isApplicationAuth) return;
 
+        synchronized (this.lock) {
             try {
-                JsonObject json = YoutubeHttpUtil.insert(form, "application/x-www-form-urlencoded", "https://oauth2.googleapis.com/token", null);
-                if (json.containsKey("error")) {
-                    throw new ApiAuthException(json.toString());
-                } else {
-                    AuthResponse data = Rson.DEFAULT.fromJson(json, AuthResponse.class);
+                Map<String, String> body = Map.of(
+                    "grant_type", "refresh_token",
+                    "refresh_token", this.data().refreshToken,
+                    "client_id", this.clientId,
+                    "client_secret", this.clientSecret
+                );
 
-                    this.accessToken = data.accessToken;
-                    this.expiresIn = data.expiresIn;
-                    this.lastRefresh = System.currentTimeMillis();
-                }
+                JsonObject json = YoutubeHttpUtil.insert(
+                    body.entrySet()
+                        .stream()
+                        .map((e) -> UriEscape.escapeUriQueryParam(e.getKey()) + "=" + UriEscape.escapeUriQueryParam(e.getValue()))
+                        .collect(Collectors.joining("&")),
+                    "application/x-www-form-urlencoded",
+                    "https://oauth2.googleapis.com/token",
+                    null
+                );
+                checkAndThrow(json);
+
+                YoutubeAuthData data = Rson.DEFAULT.fromJson(json, YoutubeAuthData.class);
+                this.dataProvider.save(data);
             } catch (ApiAuthException e) {
                 throw e;
             } catch (IOException | ApiException e) {
                 throw new ApiAuthException(e);
             }
         }
+
+    }
+
+    public String getAccessToken() throws ApiAuthException {
+        if (this.isApplicationAuth) return null;
+        synchronized (this.lock) {
+            if (this.isExpired()) {
+                this.refresh();
+            }
+            return this.data().accessToken;
+        }
     }
 
     @Override
     public boolean isApplicationAuth() {
-        return this.refreshToken == null;
+        return this.isApplicationAuth;
     }
 
     @Override
     public boolean isExpired() {
-        if (this.isApplicationAuth()) {
-            return false;
-        }
-
-        return ((System.currentTimeMillis() - this.lastRefresh) / 1000) > this.expiresIn;
-    }
-
-    public static AuthResponse authorize(String code, String redirectUri, String clientId, String clientSecret) throws IOException, ApiAuthException {
-        String form = String.format(
-            "grant_type=authorization_code"
-                + "&code=%s"
-                + "&redirect_uri=%s"
-                + "&client_id=%s"
-                + "&client_secret=%s",
-            UriEscape.escapeUriQueryParam(code),
-            UriEscape.escapeUriQueryParam(redirectUri),
-            UriEscape.escapeUriQueryParam(clientId),
-            UriEscape.escapeUriQueryParam(clientSecret)
-        );
-
-        try {
-            JsonObject json = YoutubeHttpUtil.insert(form, "application/x-www-form-urlencoded", "https://oauth2.googleapis.com/token", null);
-            if (json.containsKey("error")) {
-                throw new ApiAuthException(json.toString());
-            } else {
-                return Rson.DEFAULT.fromJson(json, AuthResponse.class);
-            }
-        } catch (ApiAuthException e) {
-            throw e;
-        } catch (IOException | ApiException e) {
-            throw new ApiAuthException(e);
+        if (this.isApplicationAuth) return false;
+        synchronized (this.lock) {
+            return ((System.currentTimeMillis() - this.data().issuedAt) / 1000) > this.data().expiresIn;
         }
     }
 
-    public static String generateOAuth2Url(@NonNull String clientId, @NonNull String redirectUri, @NonNull String scope, @NonNull String state) {
-        return String.format(
-            "https://accounts.google.com/o/oauth2/v2/auth"
-                + "?scope=%s"
-                + "&state=%s"
-                + "&redirect_uri=%s"
-                + "&response_type=code"
-                + "&prompt=consent"
-                + "&access_type=offline"
-                + "&client_id=%s",
-            UriEscape.escapeUriQueryParam(scope),
-            UriEscape.escapeUriQueryParam(state),
-            UriEscape.escapeUriQueryParam(redirectUri),
-            UriEscape.escapeUriQueryParam(clientId)
-        );
-    }
-
-    @Getter
-    @ToString
+    @EqualsAndHashCode
+    @NoArgsConstructor
     @JsonClass(exposeAll = true)
-    public static class AuthResponse {
+    public static class YoutubeAuthData {
+        @JsonField("issued_at")
+        private long issuedAt = System.currentTimeMillis();
+
         @JsonField("access_token")
         private String accessToken;
-
-        @JsonField("refresh_token")
-        private String refreshToken;
 
         @JsonField("expires_in")
         private int expiresIn;
 
+        @JsonField("refresh_token")
+        private String refreshToken;
+
+        private String scope;
+
         @JsonField("token_type")
         private String tokenType;
 
+        public static YoutubeAuthData of(String refreshToken) {
+            YoutubeAuthData d = new YoutubeAuthData();
+            d.issuedAt = 0;
+            d.refreshToken = refreshToken;
+            return d;
+        }
+
+    }
+
+    /* ---------------- */
+    /* Utils            */
+    /* ---------------- */
+
+    static void checkAndThrow(JsonObject body) throws ApiAuthException {
+        if (body.containsKey("error") || body.containsKey("errors")) {
+            throw new ApiAuthException(body.toString());
+        }
     }
 
 }
