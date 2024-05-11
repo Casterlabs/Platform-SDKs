9faf6182803d432808fdcbee24a82afd02433c53
Ender <33337309+e3ndr@users.noreply.github.com>
2024-05-10 23:59:37 -0500

    TikTok: Implement the new auth system.

--- TikTok/src/main/java/co/casterlabs/sdk/tiktok/TiktokAuth.java
+++ TikTok/src/main/java/co/casterlabs/sdk/tiktok/TiktokAuth.java
@@ -1,35 +1,149 @@
 package co.casterlabs.sdk.tiktok;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.unbescape.uri.UriEscape;
 
 import co.casterlabs.apiutil.auth.ApiAuthException;
-import co.casterlabs.apiutil.auth.OAuthProvider;
-import co.casterlabs.apiutil.auth.OAuthStrategy;
+import co.casterlabs.apiutil.auth.AuthDataProvider;
+import co.casterlabs.apiutil.auth.AuthProvider;
+import co.casterlabs.apiutil.web.RsonBodyHandler;
+import co.casterlabs.apiutil.web.WebRequest;
+import co.casterlabs.rakurai.json.Rson;
+import co.casterlabs.rakurai.json.annotating.JsonClass;
+import co.casterlabs.rakurai.json.annotating.JsonField;
+import co.casterlabs.rakurai.json.element.JsonObject;
+import co.casterlabs.sdk.tiktok.TiktokAuth.TiktokAuthData;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 
-public class TiktokAuth extends OAuthProvider {
-    private static final String TOKEN_ENDPOINT = "https://www.tiktok.com/v2/auth/authorize";
-    private static final OAuthStrategy STRATEGY = new TiktokOAuthStrategy();
+public class TiktokAuth extends AuthProvider<TiktokAuthData> {
+    private final Object lock = new Object();
 
-    public TiktokAuth(@NonNull String clientId, @NonNull String clientSecret, @NonNull String refreshToken) throws ApiAuthException {
-        super(STRATEGY, clientId, clientSecret, refreshToken, "");
+    private String clientId;
+    private String clientSecret;
+
+    /* ---------------- */
+    /* Construction     */
+    /* ---------------- */
+
+    private TiktokAuth(AuthDataProvider<TiktokAuthData> dataProvider) {
+        super(dataProvider);
     }
+
+    public static TiktokAuth ofUser(AuthDataProvider<TiktokAuthData> dataProvider, String clientId, String clientSecret) {
+        TiktokAuth auth = new TiktokAuth(dataProvider);
+        auth.clientId = clientId;
+        auth.clientSecret = clientSecret;
+        return auth;
+    }
+
+    /* ---------------- */
+    /* Impl.            */
+    /* ---------------- */
 
     @SneakyThrows
     @Override
     protected void authenticateRequest0(@NonNull HttpRequest.Builder request) {
-        request.header("Authorization", "Bearer " + this.getAccessToken());
+        request.header("Authorization", String.join(" ", this.data().tokenType, this.data().accessToken));
     }
 
     @Override
-    protected String getTokenEndpoint() {
-        return TOKEN_ENDPOINT;
+    public void refresh() throws ApiAuthException {
+        synchronized (this.lock) {
+            try {
+                Map<String, String> body = Map.of(
+                    "grant_type", "refresh_token",
+                    "refresh_token", this.data().refreshToken,
+                    "client_key", this.clientId,
+                    "client_secret", this.clientSecret
+                );
+
+                JsonObject json = WebRequest.sendHttpRequest(
+                    HttpRequest.newBuilder()
+                        .uri(URI.create("https://open.tiktokapis.com/v2/oauth/token/"))
+                        .POST(
+                            BodyPublishers.ofString(
+                                body.entrySet()
+                                    .stream()
+                                    .map((e) -> UriEscape.escapeUriQueryParam(e.getKey()) + "=" + UriEscape.escapeUriQueryParam(e.getValue()))
+                                    .collect(Collectors.joining("&"))
+                            )
+                        )
+                        .header("Content-Type", "application/x-www-form-urlencoded"),
+                    RsonBodyHandler.of(JsonObject.class),
+                    null
+                ).body();
+                checkAndThrow(json);
+
+                TiktokAuthData data = Rson.DEFAULT.fromJson(json, TiktokAuthData.class);
+                this.dataProvider.save(data);
+            } catch (IOException e) {
+                throw new ApiAuthException(e);
+            }
+        }
     }
 
-    public static OAuthResponse authorize(String code, String redirectUri, String clientId, String clientSecret) throws IOException, ApiAuthException {
-        return OAuthProvider.authorize(STRATEGY, TOKEN_ENDPOINT, code, redirectUri, clientId, clientSecret);
+    @Override
+    public boolean isApplicationAuth() {
+        return false;
+    }
+
+    @Override
+    public boolean isExpired() {
+        synchronized (this.lock) {
+            return ((System.currentTimeMillis() - this.data().issuedAt) / 1000) > this.data().expiresIn;
+        }
+    }
+
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    @JsonClass(exposeAll = true)
+    public static class TiktokAuthData {
+        private long issuedAt = System.currentTimeMillis();
+
+        @JsonField("access_token")
+        private String accessToken;
+
+        @JsonField("expires_in")
+        private int expiresIn;
+
+        @JsonField("refresh_token")
+        private String refreshToken;
+
+        private String scope;
+
+        @JsonField("token_type")
+        private String tokenType;
+
+        public static TiktokAuthData of(String refreshToken) {
+            TiktokAuthData d = new TiktokAuthData();
+            d.issuedAt = 0;
+            d.refreshToken = refreshToken;
+            return d;
+        }
+
+    }
+
+    /* ---------------- */
+    /* Utils            */
+    /* ---------------- */
+
+    static void checkAndThrow(JsonObject body) throws ApiAuthException {
+        if (body.containsKey("error_code")) {
+            int code = body.getNumber("error_code").intValue();
+
+            if (code != 0) {
+                throw new ApiAuthException(body.toString());
+            }
+        }
     }
 
 }
