cb9d389ac35e03e28fb9d08adea83a2c1003a5ea
Ender <33337309+e3ndr@users.noreply.github.com>
2024-05-25 13:08:05 -0500

    Base: Update the little helper.

--- _Base/src/main/java/co/casterlabs/apiutil/web/URIParameters.java
+++ _Base/src/main/java/co/casterlabs/apiutil/web/URIParameters.java
@@ -3,12 +3,23 @@ package co.casterlabs.apiutil.web;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.jetbrains.annotations.Nullable;
 import org.unbescape.uri.UriEscape;
 
 import lombok.NonNull;
 
 public class URIParameters {
     private List<String> parameters = new LinkedList<>();
+
+    /**
+     * Only put()s if the value isn't null.
+     */
+    public URIParameters optionalPut(@NonNull String key, @Nullable String value) {
+        if (value != null) {
+            this.put(key, value);
+        }
+        return this;
+    }
 
     public URIParameters put(@NonNull String key, @NonNull String value) {
         this.parameters.add(
