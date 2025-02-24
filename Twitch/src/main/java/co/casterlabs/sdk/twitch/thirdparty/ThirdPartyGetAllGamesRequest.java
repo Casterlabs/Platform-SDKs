46c2bcb634e05cba7c29825f00ed0a91ebaf3c34
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-02-23 22:00:51 -0600

    Twitch: Fix this request.

--- Twitch/src/main/java/co/casterlabs/sdk/twitch/thirdparty/ThirdPartyGetAllGamesRequest.java
+++ Twitch/src/main/java/co/casterlabs/sdk/twitch/thirdparty/ThirdPartyGetAllGamesRequest.java
@@ -15,7 +15,6 @@ import co.casterlabs.rakurai.json.element.JsonArray;
 import co.casterlabs.rakurai.json.element.JsonElement;
 import co.casterlabs.rakurai.json.element.JsonObject;
 import co.casterlabs.sdk.twitch.thirdparty.ThirdPartyGetAllGamesRequest.TwitchGame;
-import lombok.Getter;
 import lombok.ToString;
 
 /**
@@ -27,7 +26,7 @@ public class ThirdPartyGetAllGamesRequest extends WebRequest<List<TwitchGame>> {
     @Override
     public List<TwitchGame> execute() throws ApiException, ApiAuthException, IOException {
         JsonArray array = WebRequest.sendHttpRequest(
-            HttpRequest.newBuilder(URI.create("https://raw.githubusercontent.com/Nerothos/TwithGameList/master/game_info.json")),
+            HttpRequest.newBuilder(URI.create("https://raw.githubusercontent.com/Nerothos/TwithGameList/d5a5e7afc190cf40d4639b007be4b0dd6931e7f1/game_info.json")),
             RsonBodyHandler.of(JsonArray.class),
             null
         ).body();
@@ -46,13 +45,12 @@ public class ThirdPartyGetAllGamesRequest extends WebRequest<List<TwitchGame>> {
         return result;
     }
 
-    @Getter
     @ToString
     @JsonClass(exposeAll = true)
     public static class TwitchGame {
-        private String id;
-        private String name;
-        private String boxArtUrl;
+        public final String id;
+        public final String name;
+        public final String boxArtUrl;
 
         public TwitchGame(String id, String name) {
             this.id = id;
