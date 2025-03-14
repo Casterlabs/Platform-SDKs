172ddaa15ed5ddab3db6db9dd031d817a4e67f05
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-03-13 19:06:44 -0500

    Base: Fix clean up PaginatedResponse.

--- _Base/src/main/java/co/casterlabs/apiutil/web/PaginatedResponse.java
+++ _Base/src/main/java/co/casterlabs/apiutil/web/PaginatedResponse.java
@@ -1,11 +1,12 @@
 package co.casterlabs.apiutil.web;
 
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -15,56 +16,55 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
 @RequiredArgsConstructor
-public class PaginatedResponse<R> {
+public class PaginatedResponse<R> implements Iterable<R> {
     private static final Object INITIAL_CURSOR = new Object();
 
     private final PageFunction<R> nextPage;
-    private Object cursor = INITIAL_CURSOR;
-
-    public boolean hasNext() {
-        return this.cursor != null;
-    }
 
     public Optional<R> first() throws ApiAuthException, ApiException, IOException {
-        R[] next = this.next();
-        if (next.length == 0) {
-            return Optional.empty();
-        }
-        return Optional.of(next[0]);
+        return this.stream().findFirst();
     }
 
     public List<R> all() throws ApiAuthException, ApiException, IOException {
-        List<R> list = new LinkedList<>();
-        while (this.hasNext()) {
-            R[] next = this.next();
-            for (R r : next) {
-                list.add(r);
-            }
-        }
-        return list;
+        return this.stream()
+            .collect(Collectors.toList());
     }
 
-    public Stream<R> stream() throws ApiException, ApiAuthException, IOException {
-        return Stream.generate(new Supplier<>() {
-            private R[] current;
+    @Override
+    public Iterator<R> iterator() {
+        return new Iterator<>() {
+            private Object cursor = INITIAL_CURSOR;
+            private R[] currentPage;
             private volatile int index = 0;
 
             @SneakyThrows
             @Override
-            public synchronized R get() {
-                if (this.current == null || this.index == this.current.length) {
-                    this.current = PaginatedResponse.this.next();
+            public boolean hasNext() {
+                if (this.currentPage == null || this.index == this.currentPage.length) {
+                    if (this.cursor == null) {
+                        return false;
+                    }
+
+                    Object cursor = this.cursor == INITIAL_CURSOR ? null : this.cursor;
+                    Page<R> page = PaginatedResponse.this.nextPage.next(cursor);
+
+                    this.cursor = page.cursor;
+                    this.currentPage = page.response;
                     this.index = 0;
                 }
-                return this.current[this.index++];
+
+                return this.index != this.currentPage.length;
             }
-        });
+
+            @Override
+            public R next() {
+                return this.currentPage[this.index++];
+            }
+        };
     }
 
-    public R[] next() throws ApiException, ApiAuthException, IOException {
-        Page<R> page = this.nextPage.next(this.cursor == INITIAL_CURSOR ? null : this.cursor);
-        this.cursor = page.cursor;
-        return page.response;
+    public Stream<R> stream() {
+        return StreamSupport.stream(this.spliterator(), false);
     }
 
     @FunctionalInterface
