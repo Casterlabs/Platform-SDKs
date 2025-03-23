04033d0750cbbec5954c48b64760ad8cded7568a
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-03-23 05:49:54 -0500

    DLive: Revert changes to DliveChat.
    Throwing spaghetti at the wall...

--- DLive/src/main/java/co/casterlabs/sdk/dlive/realtime/DliveChat.java
+++ DLive/src/main/java/co/casterlabs/sdk/dlive/realtime/DliveChat.java
@@ -1,11 +1,13 @@
 package co.casterlabs.sdk.dlive.realtime;
 
-import java.io.IOException;
+import java.io.Closeable;
 import java.net.URI;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 import org.jetbrains.annotations.Nullable;
 
-import co.casterlabs.apiutil.realtime.WSConnection;
+import co.casterlabs.apiutil.auth.ApiAuthException;
 import co.casterlabs.rakurai.json.Rson;
 import co.casterlabs.rakurai.json.element.JsonElement;
 import co.casterlabs.rakurai.json.element.JsonObject;
@@ -20,11 +22,14 @@ import co.casterlabs.sdk.dlive.realtime.events._DliveChatDelete;
 import co.casterlabs.sdk.dlive.realtime.events._DliveChatFollow;
 import lombok.NonNull;
 import lombok.Setter;
-import lombok.SneakyThrows;
 import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 import xyz.e3ndr.fastloggingframework.logging.LogLevel;
 
-public class DliveChat extends WSConnection {
+public class DliveChat implements Closeable {
+    private static final String DEBUG_WS_SEND = "\u2191 %s";
+    private static final String DEBUG_WS_RECIEVE = "\u2193 %s";
+
+    private Connection conn;
     private @Setter @Nullable DliveChatListener listener;
 
     private DliveAuth auth;
@@ -33,115 +38,174 @@ public class DliveChat extends WSConnection {
     public DliveChat(@NonNull DliveAuth auth, @NonNull String username) {
         this.auth = auth;
         this.username = username;
-        this.setUri(URI.create(DliveApiJava.REALTIME_API));
     }
 
-    private void send(String type, JsonObject payload) throws IOException {
-        JsonObject frame = new JsonObject()
-            .put("type", type)
-            .put("payload", payload);
-        super.send(frame.toString());
-    }
-
-    @SneakyThrows
-    @Override
-    protected void onOpen() {
-        this.send("connection_init", JsonObject.singleton("authorization", this.auth.getAccessToken()));
-
-        if (this.listener != null) {
-            this.listener.onOpen();
+    public void connect() {
+        if (this.conn == null) {
+            this.conn = new Connection();
+            this.conn.connect();
+        } else {
+            throw new IllegalStateException("You must close the connection before reconnecting.");
         }
     }
 
+    public void connectBlocking() throws InterruptedException {
+        if (this.conn == null) {
+            this.conn = new Connection();
+            this.conn.connectBlocking();
+        } else {
+            throw new IllegalStateException("You must close the connection before reconnecting.");
+        }
+    }
+
+    public boolean isOpen() {
+        return this.conn != null;
+    }
+
     @Override
-    protected void onMessage(String raw) {
-        try {
-            JsonObject payload = Rson.DEFAULT.fromJson(raw, JsonObject.class);
+    public void close() {
+        doCleanup();
+    }
 
-            switch (payload.getString("type")) {
-                case "connection_ack": {
-                    this.send(
-                        "start",
-                        JsonObject.singleton(
-                            "query",
-                            String.format("subscription{streamMessageReceived(streamer:%s){__typename}}", new JsonString(username))
-                        )
-                    );
-                    break;
+    private void doCleanup() {
+        if (this.conn != null) {
+            try {
+                this.conn.close();
+            } catch (Exception ignored) {}
+        }
+
+        this.conn = null;
+    }
+
+    private class Connection extends WebSocketClient {
+
+        public Connection() {
+            super(URI.create(DliveApiJava.REALTIME_API));
+        }
+
+        public void send(String type, JsonObject payload) {
+            JsonObject frame = new JsonObject()
+                .put("type", type)
+                .put("payload", payload);
+
+            FastLogger.logStatic(LogLevel.TRACE, DEBUG_WS_SEND, frame);
+            super.send(frame.toString());
+        }
+
+        @Override
+        public void onOpen(ServerHandshake handshakedata) {
+            try {
+                this.send("connection_init", JsonObject.singleton("authorization", auth.getAccessToken()));
+
+                if (listener != null) {
+                    listener.onOpen();
                 }
+            } catch (ApiAuthException e) {
+                FastLogger.logException(e);
+                this.close();
+            }
+        }
 
-                case "data": {
-                    if (this.listener == null) break;
+        @Override
+        public void onMessage(String raw) {
+            FastLogger.logStatic(LogLevel.TRACE, DEBUG_WS_RECIEVE, raw);
 
-                    for (JsonElement e : payload.getObject("payload").getObject("data").getArray("streamMessageReceived")) {
-                        JsonObject item = e.getAsObject();
+            try {
+                JsonObject payload = Rson.DEFAULT.fromJson(raw, JsonObject.class);
 
-                        // https://dev.dlive.tv/schema/chattype.doc.html
-                        switch (item.getString("type")) {
+                switch (payload.getString("type")) {
+                    case "connection_ack": {
+                        this.send(
+                            "start",
+                            JsonObject.singleton(
+                                "query",
+                                String.format("subscription{streamMessageReceived(streamer:%s){__typename}}", new JsonString(username))
+                            )
+                        );
+                        break;
+                    }
 
-                            case "Message": {
-                                this.listener.onMessage(Rson.DEFAULT.fromJson(item, DliveChatMessage.class));
-                                break;
-                            }
+                    case "data": {
+                        if (listener == null) break;
 
-                            case "Gift": {
-                                this.listener.onGift(Rson.DEFAULT.fromJson(item, DliveChatGift.class));
-                                break;
-                            }
+                        for (JsonElement e : payload.getObject("payload").getObject("data").getArray("streamMessageReceived")) {
+                            JsonObject item = e.getAsObject();
 
-                            case "Live": {
-                                this.listener.onLive();
-                                break;
-                            }
+                            // https://dev.dlive.tv/schema/chattype.doc.html
+                            switch (item.getString("type")) {
 
-                            case "Offline": {
-                                this.listener.onOffline();
-                                break;
-                            }
+                                case "Message": {
+                                    listener.onMessage(Rson.DEFAULT.fromJson(item, DliveChatMessage.class));
+                                    break;
+                                }
 
-                            case "Follow": {
-                                this.listener.onFollow(Rson.DEFAULT.fromJson(item, _DliveChatFollow.class).sender);
-                                break;
-                            }
+                                case "Gift": {
+                                    listener.onGift(Rson.DEFAULT.fromJson(item, DliveChatGift.class));
+                                    break;
+                                }
 
-                            case "Subscription": {
-                                this.listener.onSubscription(Rson.DEFAULT.fromJson(item, DliveChatSubscription.class));
-                                break;
-                            }
+                                case "Live": {
+                                    listener.onLive();
+                                    break;
+                                }
 
-                            case "Delete": {
-                                this.listener.onMessagesDelete(Rson.DEFAULT.fromJson(item, _DliveChatDelete.class).ids);
-                                break;
-                            }
+                                case "Offline": {
+                                    listener.onOffline();
+                                    break;
+                                }
 
-                            case "Host": {
-                                this.listener.onHost(Rson.DEFAULT.fromJson(item, DliveChatHost.class));
-                                break;
-                            }
+                                case "Follow": {
+                                    listener.onFollow(Rson.DEFAULT.fromJson(item, _DliveChatFollow.class).sender);
+                                    break;
+                                }
 
-                            // Unused.
-                            case "ChatMode":
-                            case "Ban":
-                            case "Mod":
-                            case "Emote":
-                            case "Timeout": {
-                                break;
+                                case "Subscription": {
+                                    listener.onSubscription(Rson.DEFAULT.fromJson(item, DliveChatSubscription.class));
+                                    break;
+                                }
+
+                                case "Delete": {
+                                    listener.onMessagesDelete(Rson.DEFAULT.fromJson(item, _DliveChatDelete.class).ids);
+                                    break;
+                                }
+
+                                case "Host": {
+                                    listener.onHost(Rson.DEFAULT.fromJson(item, DliveChatHost.class));
+                                    break;
+                                }
+
+                                // Unused.
+                                case "ChatMode":
+                                case "Ban":
+                                case "Mod":
+                                case "Emote":
+                                case "Timeout": {
+                                    break;
+                                }
                             }
                         }
+                        break;
                     }
-                    break;
                 }
+            } catch (Throwable t) {
+                FastLogger.logStatic(LogLevel.SEVERE, "An uncaught exception occurred whilst processing frame: %s\n%s", raw, t);
             }
-        } catch (Throwable t) {
-            FastLogger.logStatic(LogLevel.SEVERE, "An uncaught exception occurred whilst processing frame: %s\n%s", raw, t);
         }
-    }
 
-    @Override
-    protected void onClose(boolean remote) {
-        if (this.listener != null) {
-            this.listener.onClose(remote);
+        @Override
+        public void onClose(int code, String reason, boolean remote) {
+            doCleanup();
+
+            if (listener != null) {
+                new Thread(() -> listener.onClose(remote));
+            }
         }
+
+        @Override
+        public void onError(Exception e) {
+            FastLogger.logStatic(LogLevel.SEVERE, "An uncaught exception occurred:\n%s", e);
+        }
+
     }
 
 }
