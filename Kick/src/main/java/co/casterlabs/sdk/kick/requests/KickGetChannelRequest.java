8ea82965c1e2f1a1f0123b928bc8443f7dd8a5a6
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-04-20 11:05:15 -0500

    Kick: GetChannelRequest can now do slugs.

--- Kick/src/main/java/co/casterlabs/sdk/kick/requests/KickGetChannelRequest.java
+++ Kick/src/main/java/co/casterlabs/sdk/kick/requests/KickGetChannelRequest.java
@@ -12,27 +12,34 @@ import co.casterlabs.apiutil.web.AuthenticatedWebRequest;
 import co.casterlabs.sdk.kick.KickAuth;
 import co.casterlabs.sdk.kick.types.KickChannel;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 
-@Accessors(chain = true, fluent = true)
 public class KickGetChannelRequest extends AuthenticatedWebRequest<KickChannel, KickAuth> {
-    private @Setter Integer forId = null;
+    private String query;
 
     public KickGetChannelRequest(@NonNull KickAuth auth) {
         super(auth);
     }
 
+    public KickGetChannelRequest forId(int id) {
+        this.query = "?broadcaster_user_id=" + id;
+        return this;
+    }
+
+    public KickGetChannelRequest forSlug(String slug) {
+        this.query = "?slug=" + UriEscape.escapeUriQueryParam(slug);
+        return this;
+    }
+
     public KickGetChannelRequest me() {
-        this.forId = null;
+        this.query = null;
         return this;
     }
 
     @Override
     protected KickChannel execute() throws ApiException, ApiAuthException, IOException {
         String url = "https://api.kick.com/public/v1/channels";
-        if (this.forId != null) {
-            url += "?broadcaster_user_id=" + UriEscape.escapeUriQueryParam(String.valueOf(this.forId));
+        if (this.query != null) {
+            url += this.query;
         }
 
         return _KickApi.request(
