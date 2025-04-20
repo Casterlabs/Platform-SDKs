cee93ac1f70849798a014c1ad1c8f18c4f11d69a
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-04-20 11:31:58 -0500

    Kick: User identity!

--- Kick/src/main/java/co/casterlabs/sdk/kick/webhook/v1/types/KickWebhookUserV1.java
+++ Kick/src/main/java/co/casterlabs/sdk/kick/webhook/v1/types/KickWebhookUserV1.java
@@ -1,5 +1,7 @@
 package co.casterlabs.sdk.kick.webhook.v1.types;
 
+import org.jetbrains.annotations.Nullable;
+
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import co.casterlabs.rakurai.json.annotating.JsonField;
 
@@ -22,5 +24,29 @@ public class KickWebhookUserV1 {
 
     @JsonField("profile_picture")
     public final String profilePictureUrl = null;
+
+    /**
+     * @apiNote null if:
+     *          <ul>
+     *          <li>the user type is for a broadcaster.</li>
+     *          <li>the user is anonymous.</li>
+     *          <li><i>any undocumented edge-case.</i></li>
+     *          </ul>
+     */
+    public final @Nullable Identity identity = null;
+
+    @JsonClass(exposeAll = true)
+    public class Identity {
+
+        @JsonField("username_color")
+        public final String color = null;
+
+        @JsonClass(exposeAll = true)
+        public static class Badge {
+            public final String text = null;
+            public final String type = null;
+        }
+
+    }
 
 }
