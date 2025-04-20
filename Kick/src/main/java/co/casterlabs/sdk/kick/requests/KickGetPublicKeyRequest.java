179fa319d58c073c69921020f116f4fa4a35fcc5
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-04-20 11:12:38 -0500

    Kick: This no longer needs auth!

--- Kick/src/main/java/co/casterlabs/sdk/kick/requests/KickGetPublicKeyRequest.java
+++ Kick/src/main/java/co/casterlabs/sdk/kick/requests/KickGetPublicKeyRequest.java
@@ -4,28 +4,21 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
 
-import co.casterlabs.apiutil.auth.ApiAuthException;
 import co.casterlabs.apiutil.web.ApiException;
-import co.casterlabs.apiutil.web.AuthenticatedWebRequest;
+import co.casterlabs.apiutil.web.WebRequest;
 import co.casterlabs.rakurai.json.element.JsonObject;
-import co.casterlabs.sdk.kick.KickAuth;
-import lombok.NonNull;
 import lombok.experimental.Accessors;
 
 @Accessors(chain = true, fluent = true)
-public class KickGetPublicKeyRequest extends AuthenticatedWebRequest<String, KickAuth> {
-
-    public KickGetPublicKeyRequest(@NonNull KickAuth auth) {
-        super(auth);
-    }
+public class KickGetPublicKeyRequest extends WebRequest<String> {
 
     @Override
-    protected String execute() throws ApiException, ApiAuthException, IOException {
+    protected String execute() throws ApiException, IOException {
         String url = "https://api.kick.com/public/v1/public-key";
 
         return _KickApi.request(
             HttpRequest.newBuilder(URI.create(url)),
-            this.auth,
+            null,
             JsonObject.class
         ).getString("public_key");
     }
