8ab9ff7e18d6bc222e58ff90b9c5ecea0bb3b22c
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-04-20 11:21:50 -0500

    Base: Make URIParameters more flexible.

--- _Base/src/main/java/co/casterlabs/apiutil/web/URIParameters.java
+++ _Base/src/main/java/co/casterlabs/apiutil/web/URIParameters.java
@@ -1,5 +1,6 @@
 package co.casterlabs.apiutil.web;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,30 +15,30 @@ public class URIParameters {
     /**
      * Only put()s if the value isn't null.
      */
-    public URIParameters optionalPut(@NonNull String key, @Nullable String value) {
+    public URIParameters optionalPut(@NonNull String key, @Nullable Object value) {
         if (value != null) {
-            this.put(key, value);
+            this.put(key, String.valueOf(value));
         }
         return this;
     }
 
-    public URIParameters put(@NonNull String key, @NonNull String value) {
+    public URIParameters put(@NonNull String key, @NonNull Object value) {
         this.parameters.add(
             UriEscape.escapeUriQueryParam(key) + '=' +
-                UriEscape.escapeUriQueryParam(value)
+                UriEscape.escapeUriQueryParam(String.valueOf(value))
         );
         return this;
     }
 
-    public URIParameters putAllAsSeparateKeys(@NonNull String key, @NonNull List<String> values) {
-        for (String value : values) {
+    public URIParameters putAllAsSeparateKeys(@NonNull String key, @NonNull Collection<Object> values) {
+        for (Object value : values) {
             this.put(key, value);
         }
         return this;
     }
 
-    public URIParameters putAllAsSeparateKeys(@NonNull String key, @NonNull String[] values) {
-        for (String value : values) {
+    public URIParameters putAllAsSeparateKeys(@NonNull String key, @NonNull Object... values) {
+        for (Object value : values) {
             this.put(key, value);
         }
         return this;
