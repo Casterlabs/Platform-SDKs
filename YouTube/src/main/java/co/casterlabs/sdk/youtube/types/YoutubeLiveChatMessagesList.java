8eb3dd5c373e28b181afc079afedaa13766a0187
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-05-13 09:37:19 -0500

    YouTube: Optimize the deserialization of live chat items.

--- YouTube/src/main/java/co/casterlabs/sdk/youtube/types/YoutubeLiveChatMessagesList.java
+++ YouTube/src/main/java/co/casterlabs/sdk/youtube/types/YoutubeLiveChatMessagesList.java
@@ -1,9 +1,9 @@
 package co.casterlabs.sdk.youtube.types;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import co.casterlabs.rakurai.json.Rson;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
@@ -30,16 +30,30 @@ public class YoutubeLiveChatMessagesList {
     private void $deserialize_items(JsonElement itemsElement) throws JsonValidationException, JsonParseException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         JsonArray items = itemsElement.getAsArray();
 
-        List<YoutubeLiveChatEvent> events = new ArrayList<>(items.size());
-
-        for (JsonElement item : items) {
-            item.getAsObject().put("isHistorical", this.isHistorical);
-            events.add(Rson.DEFAULT.fromJson(item, YoutubeLiveChatEvent.class));
+        List<YoutubeLiveChatEvent> events;
+        if (items.isEmpty()) {
+            events = Collections.emptyList();
+        } else {
+            events = items.toList()
+                .stream()
+                .map((e) -> e.getAsObject())
+                .map((item) -> {
+                    // Inject the historical tag.
+                    return item.put("isHistorical", this.isHistorical);
+                })
+                .map((item) -> {
+                    try {
+                        return Rson.DEFAULT.fromJson(item, YoutubeLiveChatEvent.class);
+                    } catch (JsonParseException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(Collectors.toList());
         }
 
         Field f = YoutubeLiveChatMessagesList.class.getField("events");
         f.setAccessible(true);
-        f.set(this, Collections.unmodifiableList(events));
+        f.set(this, events);
     }
 
     public boolean isEmpty() {
