194780f03283a9fad968ff4c90e2c7b068445024
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-10-16 16:06:58 -0500

    YouTube: Start & End :p

--- YouTube/src/main/java/co/casterlabs/sdk/youtube/types/YoutubeLiveBroadcastSnippet.java
+++ YouTube/src/main/java/co/casterlabs/sdk/youtube/types/YoutubeLiveBroadcastSnippet.java
@@ -13,9 +13,14 @@ public class YoutubeLiveBroadcastSnippet {
     public final String channelId = null;
     public final String description = null;
     public final Instant publishedAt = null;
-    public final Instant scheduledStartTime = null;
     public final Boolean isDefaultBroadcast = null;
     public final String liveChatId = null;
+
+    public final Instant scheduledStartTime = null;
+    public final Instant scheduledEndTime = null;
+
+    public final Instant actualStartTime = null;
+    public final Instant actualEndTime = null;
 
     public final Map<String, YoutubeThumbnail> thumbnails = null;
 
