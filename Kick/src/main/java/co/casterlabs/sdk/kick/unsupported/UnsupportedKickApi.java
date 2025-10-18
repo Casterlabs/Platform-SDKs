ae78793ec6e995eef1a8a53f585a711f8fb63f5c
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-10-18 15:41:19 -0500

    Kick: Check the safety of that :p

--- Kick/src/main/java/co/casterlabs/sdk/kick/unsupported/UnsupportedKickApi.java
+++ Kick/src/main/java/co/casterlabs/sdk/kick/unsupported/UnsupportedKickApi.java
@@ -1,5 +1,7 @@
 package co.casterlabs.sdk.kick.unsupported;
 
+import org.jetbrains.annotations.Nullable;
+
 import co.casterlabs.rakurai.json.element.JsonElement;
 import co.casterlabs.rakurai.json.element.JsonObject;
 
@@ -8,7 +10,7 @@ public class UnsupportedKickApi {
     public static String PUSHER_CLUSTER = "us2";
     public static String PUSHER_KEY = "32cbd69e4b950bf97679";
 
-    public static String parseResponsiveImage(JsonElement e) {
+    public static @Nullable String parseResponsiveImage(JsonElement e) {
         if ((e == null) || e.isJsonNull()) {
             return null;
         } else if (e.isJsonString()) {
@@ -17,17 +19,41 @@ public class UnsupportedKickApi {
 
         JsonObject obj = e.getAsObject();
 
+        try {
+            if (obj.containsKey("responsive")) {
+                return splitImage(obj.getString("responsive"));
+            }
+        } catch (Throwable ignored) {}
+
+        try {
+            if (obj.containsKey("srcset")) {
+                return splitImage(obj.getString("srcset"));
+            }
+        } catch (Throwable ignored) {}
+
+        try {
+            if (obj.containsKey("url")) {
+                return obj.getString("url");
+            }
+        } catch (Throwable ignored) {}
+
+        try {
+            if (obj.containsKey("src")) {
+                return obj.getString("src");
+            }
+        } catch (Throwable ignored) {}
+
+        return null;
+    }
+
+    private static String splitImage(String responsive) {
         // The string kinda looks like this:
         // https://stream.kick.com/thumbnails/[...].webp 1920w, [...]
-        String responsive = obj.getString("responsive");
-        if (responsive != null) {
-            int firstSpaceAt = responsive.indexOf(' ');
-            if (firstSpaceAt != -1) {
-                return responsive.substring(0, firstSpaceAt);
-            }
+        int firstSpaceAt = responsive.indexOf(' ');
+        if (firstSpaceAt != -1) {
+            return responsive.substring(0, firstSpaceAt);
         }
-
-        return obj.getString("url"); // fallback.
+        throw new IllegalArgumentException();
     }
 
 }
