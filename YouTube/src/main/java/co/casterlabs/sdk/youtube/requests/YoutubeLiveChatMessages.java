ad6a6b43e435ce3d1c56a539b8de82e39fb0a8db
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-12-14 10:04:10 -0600

    YouTube: LiveChatMessages.StreamList :)
    New API. It kinda sucks, but whatever. We'll see how bad the quota impact is.

--- YouTube/src/main/java/co/casterlabs/sdk/youtube/requests/YoutubeLiveChatMessages.java
+++ YouTube/src/main/java/co/casterlabs/sdk/youtube/requests/YoutubeLiveChatMessages.java
@@ -1,7 +1,11 @@
 package co.casterlabs.sdk.youtube.requests;
 
+import java.io.Closeable;
+import java.util.Iterator;
+
 import org.jetbrains.annotations.Nullable;
 
+import co.casterlabs.apiutil.auth.ApiAuthException;
 import co.casterlabs.apiutil.web.QueryBuilder;
 import co.casterlabs.rakurai.json.Rson;
 import co.casterlabs.rakurai.json.element.JsonObject;
@@ -10,6 +14,15 @@ import co.casterlabs.rakurai.json.validation.JsonValidationException;
 import co.casterlabs.sdk.youtube.YoutubeApiRequest;
 import co.casterlabs.sdk.youtube.YoutubeAuth;
 import co.casterlabs.sdk.youtube.types.YoutubeLiveChatMessagesList;
+import co.casterlabs.sdk.youtube.types.protobuf.LiveChatMessage;
+import co.casterlabs.sdk.youtube.types.protobuf.LiveChatMessageListRequest;
+import co.casterlabs.sdk.youtube.types.protobuf.LiveChatMessageListResponse;
+import co.casterlabs.sdk.youtube.types.protobuf.V3DataLiveChatMessageServiceGrpc;
+import co.casterlabs.sdk.youtube.types.protobuf.V3DataLiveChatMessageServiceGrpc.V3DataLiveChatMessageServiceBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Metadata;
+import io.grpc.stub.MetadataUtils;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -127,7 +140,7 @@ public class YoutubeLiveChatMessages {
             return QueryBuilder.from(
                 "part", "snippet,authorDetails",
                 "profileImageSize", "88",
-//	              "maxResults", "2000",
+//                "maxResults", "2000",
                 "liveChatId", this.forLiveChatId,
                 "pageToken", this.withPageToken // optional
             );
@@ -138,6 +151,93 @@ public class YoutubeLiveChatMessages {
             json.put("isHistorical", this.withPageToken == null);
             return Rson.DEFAULT.fromJson(json, YoutubeLiveChatMessagesList.class);
         }
+    }
+
+    public static class StreamList implements Closeable {
+        private static final Metadata.Key<String> API_KEY_METADATA = Metadata.Key.of("x-goog-api-key", Metadata.ASCII_STRING_MARSHALLER);
+        private static final Metadata.Key<String> AUTH_METADATA = Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER);
+
+        private final Metadata authMetadata;
+        private final LiveChatMessageListRequest.Builder request;
+        private final Listener listener;
+
+        private boolean shouldRun = false;
+
+        public StreamList(@NonNull YoutubeAuth auth, @NonNull String liveChatId, @NonNull Listener listener) throws ApiAuthException {
+            this.authMetadata = new Metadata();
+            if (auth.isApplicationAuth()) {
+                this.authMetadata.put(API_KEY_METADATA, auth.getApiKey());
+            } else {
+                this.authMetadata.put(AUTH_METADATA, "Bearer " + auth.getAccessToken());
+            }
+
+            this.listener = listener;
+            this.request = LiveChatMessageListRequest.newBuilder()
+                .addPart("id")
+                .addPart("snippet")
+                .addPart("authorDetails")
+                .setLiveChatId(liveChatId)
+                .setMaxResults(500);
+        }
+
+        public void run() throws ApiAuthException {
+            if (this.shouldRun) {
+                throw new IllegalStateException("Stream is already running");
+            }
+
+            ManagedChannel channel = ManagedChannelBuilder.forTarget("dns:///youtube.googleapis.com:443")
+                .useTransportSecurity()
+                .intercept(MetadataUtils.newAttachHeadersInterceptor(this.authMetadata))
+                .build();
+
+            try {
+                this.shouldRun = true;
+                this.listener.onOpen();
+
+                V3DataLiveChatMessageServiceBlockingStub stub = V3DataLiveChatMessageServiceGrpc.newBlockingStub(channel);
+
+                // TODO figure out _why_ youtube kills the stream after exactly 10 seconds.
+                while (this.shouldRun) {
+                    Iterator<LiveChatMessageListResponse> it = stub.streamList(this.request.build());
+                    while (it.hasNext()) {
+                        LiveChatMessageListResponse resp = it.next();
+                        this.request.setPageToken(resp.getNextPageToken());
+
+                        for (LiveChatMessage message : resp.getItemsList()) {
+                            this.listener.onEvent(message);
+                        }
+                    }
+                }
+            } catch (Throwable t) {
+                this.listener.onError(t);
+            } finally {
+                channel.shutdownNow();
+                this.listener.onClose();
+                this.shouldRun = false;
+            }
+        }
+
+        public boolean isRunning() {
+            return this.shouldRun;
+        }
+
+        @Override
+        public void close() {
+            this.shouldRun = false;
+        }
+
+        public static interface Listener {
+
+            default void onOpen() {}
+
+            default void onClose() {}
+
+            default void onError(Throwable t) {}
+
+            default void onEvent(LiveChatMessage message) {}
+
+        }
+
     }
 
 }
---
+++ YouTube/src/main/proto/README.md
@@ -0,0 +1,9 @@
+# YouTube LiveChatMessages StreamingLiveChat
+
+Source: https://developers.google.com/youtube/v3/live/streaming-live-chat#stream_listproto
+
+Be sure to add the following to each file when you update the .proto:
+```
+option java_multiple_files = true;
+option java_package = "co.casterlabs.sdk.youtube.types.protobuf.<filename>";
+```
