5ccd4cf969b48af1d5ec17ee443ec27f3e95967f
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-12-18 06:02:24 -0600

    Kick: Fix that.

--- Kick/src/main/java/co/casterlabs/sdk/kick/webhook/v1/types/KickWebhookChatMessageV1.java
+++ Kick/src/main/java/co/casterlabs/sdk/kick/webhook/v1/types/KickWebhookChatMessageV1.java
@@ -1,5 +1,7 @@
 package co.casterlabs.sdk.kick.webhook.v1.types;
 
+import java.util.List;
+
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import co.casterlabs.rakurai.json.annotating.JsonField;
 import co.casterlabs.sdk.kick.webhook.KickWebhookEvent;
@@ -19,28 +21,28 @@ public class KickWebhookChatMessageV1 implements KickWebhookEvent {
 
     public final String content = null;
 
-//    public final List<Emote> emotes = null;
+    public final List<Emote> emotes = null;
 
-//    @JsonClass(exposeAll = true)
-//    public static class Emote {
-//
-//        @JsonField("emote_id")
-//        public final String id = null;
-//
-//        public final List<Position> positions = null;
-//
-//        @JsonClass(exposeAll = true)
-//        public class Position {
-//
-//            @JsonField("s")
-//            public final Integer start = null;
-//
-//            @JsonField("e")
-//            public final Integer end = null;
-//
-//        }
-//
-//    }
+    @JsonClass(exposeAll = true)
+    public static class Emote {
+
+        @JsonField("emote_id")
+        public final String id = null;
+
+        public final List<Position> positions = null;
+
+        @JsonClass(exposeAll = true)
+        public static class Position {
+
+            @JsonField("s")
+            public final Integer start = null;
+
+            @JsonField("e")
+            public final Integer end = null;
+
+        }
+
+    }
 
     @Override
     public Type type() {
