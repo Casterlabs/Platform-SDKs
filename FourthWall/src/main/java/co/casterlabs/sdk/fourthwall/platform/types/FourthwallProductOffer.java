7b4c0584979eac5fd8d4fcf04ec8c0c9f4909cb3
e3ndr <33337309+e3ndr@users.noreply.github.com>
2026-06-04 11:28:17 -0500

    FourthWall: Order product offer primary image.

--- FourthWall/src/main/java/co/casterlabs/sdk/fourthwall/platform/types/FourthwallProductOffer.java
+++ FourthWall/src/main/java/co/casterlabs/sdk/fourthwall/platform/types/FourthwallProductOffer.java
@@ -11,5 +11,17 @@ public class FourthwallProductOffer {
     public final String slug = null;
     public final String description = null;
     public final FourthwallProductVariant variant = null;
+    public final PrimaryImage primaryImage = null;
+
+    @ToString
+    @JsonClass(exposeAll = true)
+    public static class PrimaryImage {
+        public final String id = null;
+        public final String url = null;
+        public final Integer width = null;
+        public final Integer height = null;
+        public final String transformedUrl = null;
+
+    }
 
 }
